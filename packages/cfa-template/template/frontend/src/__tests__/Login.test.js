588a1e2ca360a69e389ce7f96f465984deb80896
Martin Cappi Dahl <mcappi91@gmail.com>
2022-07-26 18:39:17 -0300

    wip: login unit test

--- packages/cfa-template/template/frontend/src/__tests__/Login.test.js
+++ packages/cfa-template/template/frontend/src/__tests__/Login.test.js
@@ -1,43 +1,95 @@
-import { render, screen, cleanup } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-import Login from '../components/Login';
+/**
+ * @jest-environment jsdom
+ */
+import "@babel/polyfill"; // otherwise referenceError: regeneratorRuntime is not defined
+import path from "path";
+import { fireEvent, render, screen, act } from "@testing-library/react";
+import nock from "nock";
+import userEvent from "@testing-library/user-event";
+import fetch from "node-fetch";
+import { Login } from "../components/Login";
 
-afterEach(() => {
-	cleanup();
-});
+jest.mock("node-fetch");
 
-test('logs with demo email and password', async () => {
-	const username = 'demo@lefhook.com';
-	const password = 'demo';
-	const mockLogin = jest.fn();
+// use for printing the component's HTML
+// screen.debug();
 
-	render(<Login />);
+describe("Login component", () => {
+    beforeAll(() => {
+        nock.back.fixtures = path.join(__dirname, "nockFixtures");
+    });
 
-	const emailInput = screen.getByTestId('email-input');
-	userEvent.type(emailInput, username);
-	const passwordInput = screen.getByTestId('email-input');
-	userEvent.type(passwordInput, password);
-	const loginButton = screen.getByTestId('login-button');
-	expect(loginButton).not.toBeDisabled();
+    afterEach(() => {
+        nock.restore(); // see Memory issues with Jest https://github.com/nock/nock#memory-issues-with-jest
+    });
 
-	userEvent.click(loginButton);
+    it("renders create account option", () => {
+        const { queryByText, getByText } = render(<Login />);
 
-	await expect(mockLogin).toHaveBeenCalled();
-	await expect(mockLogin).toHaveBeenCalledTimes(1);
-	await expect(mockLogin).toHaveBeenCalledWith('demo@lefhook.com', 'demo');
-});
+        expect(queryByText(/Create account./i)).toBeTruthy();
+        fireEvent.click(getByText(/Create account./i));
+    });
 
-test('matches snapshot: Login.test.js', () => {
-	const username = 'demo@lefhook.com';
-	const password = 'demo';
+    it("creates a demo user - nock intercepts", async () => {
+        const scope = nock("http://localhost:3001/dev/user/")
+            .post(/create./)
+            .reply(200, {
+                token: "eyJpZCI6IjYyZTA0NGM4ZWJhNjgxYjE1MzRkMTNmYSIsInRva2VuIjoiNWM4NTNhNWRhM2I1Yzk2MTA1ZDU5MDlkOGMzYTQzZTUwNWJkMjIwNCJ9",
+            });
 
-	const emailInput = screen.getByTestId('email-input');
-	userEvent.type(emailInput, username);
-	const passwordInput = screen.getByTestId('email-input');
-	userEvent.type(passwordInput, password);
-	const loginButton = screen.getByTestId('login-button');
-	expect(loginButton).not.toBeDisabled();
+        const { queryByText, getByText, findByText } = render(
+            <Login authToken={null} />
+        );
 
-	const tree = renderer.create(<Login />).toJSON();
-	expect(tree).toMatchSnapshot();
+        expect(queryByText(/Create account./i)).toBeTruthy();
+        await userEvent.click(getByText(/Create account./i));
+
+        expect(fetch).toHaveBeenCalled();
+        scope.isDone();
+
+        expect(fetch).toHaveBeenCalledWith(
+            expect.stringMatching(/user\/create/),
+            expect.objectContaining({
+                body: '{"username":"demo@lefthook.com","password":"demo"}',
+            })
+        );
+
+        // probably toast exists somewhere outside the login component
+        // const toastAlert = await findByText(/wurst./);
+        // expect(toastAlert).toBeInTheDocument();
+    });
+
+    it.skip("creates a demo user - jest mocks fetch", async () => {
+        const promise = Promise.resolve({
+            status: 200,
+            data: {
+                token: "eyJpZCI6IjYyZTA0NGM4ZWJhNjgxYjE1MzRkMTNmYSIsInRva2VuIjoiNWM4NTNhNWRhM2I1Yzk2MTA1ZDU5MDlkOGMzYTQzZTUwNWJkMjIwNCJ9",
+            },
+        });
+        fetch.mockImplementationOnce(() => promise);
+
+        const { queryByText, getByText, findByText } = render(
+            <Login authToken={null} />
+        );
+
+        expect(queryByText(/Create account./i)).toBeTruthy();
+        await userEvent.click(getByText(/Create account./i));
+        await act(() => promise);
+        expect(fetch).toHaveBeenCalled();
+        // expect(fetch).toHaveBeenCalledWith("undefined/user/create", {
+        //     body: '{"username":"demo@lefthook.com","password":"demo"}',
+        //     headers: { "Content-Type": "application/json" },
+        //     method: "POST",
+        // });
+        expect(fetch).toHaveBeenCalledWith(
+            expect.stringMatching(/user\/create/),
+            expect.objectContaining({
+                body: '{"username":"demo@lefthook.com","password":"demo"}',
+            })
+        );
+
+        // probably toast exists somewhere outside the login component
+        // const toastAlert = await findByText(/wurst./);
+        // expect(toastAlert).toBeInTheDocument();
+    });
 });
