4e90ccecb9e053779cd3b8f95cf27389fb049202
gregoryfeels <gregorio.martin@southworks.com>
2022-08-01 22:43:27 -0300

    Process tests for routes

---
+++ packages/cfa-template/template/backend/test/process-tests/example.process.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+const { createApp } = require('../../app');
+const user = require('../../src/routers/user');
+const UserManager = require('../../src/managers/UserManager')
+
+const app = createApp((app) => {
+    app.use(user);
+});
+
+jest.mock('../../src/managers/UserManager', () => {
+    const UserManagerMocked = {
+        createUserToken: jest.fn()
+            .mockResolvedValue('token for testing'),
+    };
+
+    return {
+        loginUser: jest.fn(() => UserManagerMocked)
+    };
+});
+
+describe('Users process tests', () => {
+    it('Test test', async () => {
+        let credentials = { username: 'yolo', password: 'wololo' };
+
+        await request(app)
+            .post('/user/login')
+            .send(credentials)
+            .then((response) => {
+                expect(response.statusCode).toEqual(201);
+                expect(response.body.token).toEqual('token for testing');
+            });
+    });
+});
