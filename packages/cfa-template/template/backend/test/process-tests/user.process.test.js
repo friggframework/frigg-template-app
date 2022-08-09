fbd3c9306675997b0a42ed198ed67f232099f15c
gregoryfeels <gregorio.martin@southworks.com>
2022-08-09 15:54:29 -0300

    Finished tests

--- packages/cfa-template/template/backend/test/process-tests/user.process.test.js
+++ packages/cfa-template/template/backend/test/process-tests/user.process.test.js
@@ -4,37 +4,8 @@ const { createApp } = require('../../app');
 const userRoute = require('../../src/routers/user');
 const authRoute = require('../../src/routers/auth');
 const demoRoute = require('../../src/routers/demo');
+const ModuleManager = require('@friggframework/api-module-salesforce/manager')
 const { default: mongoose } = require('mongoose');
-const jsforce = require('jsforce');
-
-// jest.mock('@friggframework/assertions', () => {
-//     const lodashGet = require('lodash.get');
-//     const originalModule = jest.requireActual('@friggframework/assertions');
-//     const get = (o, key, defaultValue) => {
-//         const value = lodashGet(o, key, defaultValue);
-
-//         if (value !== undefined) {
-//             return value;
-//         }
-
-//         if (key == 'access_token') return 'salesforce_access_token';
-
-//         if (defaultValue === undefined) {
-//             throw new Error({
-//                 parent: this,
-//                 key: key,
-//             });
-//         }
-
-//         return defaultValue;
-//     };
-
-//     return {
-//         __esModule: true,
-//         ...originalModule,
-//         get,
-//     };
-// });
 
 const app = createApp((app) => {
     app.use(userRoute);
@@ -42,15 +13,11 @@ const app = createApp((app) => {
     app.use(demoRoute);
 });
 
-// const authSpy = jest
-//     .spyOn(jsforce.Connection.prototype, 'authorize')
-//     .mockImplementation(() => {
-//         return Promise.resolve({
-//             id: 'salesforce_user_id',
-//             organizationId: 'salesforce_organization_id',
-//             url: 'salesforce_org_url',
-//         });
-//     });
+jest
+    .spyOn(ModuleManager.prototype, 'processAuthorizationCallback')
+    .mockImplementation(() => {
+        return 'Hello this is a token';
+    });
 
 let getIntegrationsBaselineResponse = {
     entities: {
@@ -80,14 +47,14 @@ describe('Users process tests', () => {
     let apiExternalAuthorizationUrl; // ??
 
     beforeAll(() => {
-        mongoose.connect('mongodb://localhost:27017/');
+        mongoose.connect(process.env.MONGO_URI);
     });
 
     afterAll(() => {
         mongoose.disconnect();
     });
 
-    it.skip('Creates a user', async () => {
+    it('Creates a user', async () => {
         await request(app)
             .post('/user/create')
             .send(credentials)
@@ -110,7 +77,7 @@ describe('Users process tests', () => {
             });
     });
 
-    it.skip('Lists user baseline integrations', async () => {
+    it('Lists user baseline integrations', async () => {
         await request(app)
             .get('/api/integrations')
             .set('Authorization', 'Bearer ' + token)
@@ -137,18 +104,6 @@ describe('Users process tests', () => {
     });
 
     it('Registers API authorization process', async () => {
-        const scope = nock('https://test.salesforce.com/services/oauth2')
-            .post('/token')
-            .once()
-            .reply(200, {
-                id: 'salesforce_user_id/organization_id',
-                organizationId: 'salesforce_organization_id',
-                url: 'salesforce_org_url',
-                access_token: 'some_access_token',
-                refresh_token: 'some_refresh_token',
-                instance_url: process.env.REDIRECT_URI,
-            });
-
         const payload = {
             entityType: 'salesforce',
             data: {
@@ -162,34 +117,7 @@ describe('Users process tests', () => {
             .set('Referer', 'http://localhost:3000/')
             .send(payload)
             .then((res) => {
-                // expect(authSpy).toHaveBeenCalled();
                 expect(res.statusCode).toEqual(200);
             });
-        scope.isDone();
-        /**
-        console.error when using authSpy + mocked get method
-            Error: Invalid URI "/services/data/v42.0/sobjects/Organization/describe"
-            at Request.Object.<anonymous>.Request.init (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\request\request.js:273:31)
-            at new Request (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\request\request.js:127:8)
-            at request (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\request\index.js:53:10)
-            at C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\request\index.js:100:12
-            at createRequest (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\transport.js:84:13)
-            at Promise.promise.then (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\transport.js:43:5)
-            at HttpApi.Object.<anonymous>.HttpApi.request (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\http-api.js:82:47)
-            at Object.<anonymous>.module.exports.Object.<anonymous>.Connection.request (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\connection.js:355:18)
-            at Object.<anonymous>.module.exports.Object.<anonymous>.Connection.describe.Connection.describeSObject (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\connection.js:1195:15)
-            at Object.<anonymous>.module.exports.$fn (C:\repos\create-frigg-app\packages\cfa-template\template\backend\node_modules\jsforce\lib\cache.js:239:10)
-        */
-        /**
-        console.error when using nock method
-            Error: connect ECONNREFUSED 127.0.0.1:3000
-                at TCPConnectWrap.afterConnect [as oncomplete] (node:net:1161:16) {
-                errno: -4078,
-                code: 'ECONNREFUSED',
-                syscall: 'connect',
-                address: '127.0.0.1',
-                port: 3000
-            }
-        */
     });
 });
