a4dab9cd33d22173398fdb45bc39a777f723cd95
Ryan Morlok <ryan.morlok@morlok.com>
2023-08-18 21:17:27 -0500

    Fixed issue with starting the app after creating using create-frigg-app

--- packages/cfa-template/template/frontend/src/actions/integrations.js
+++ packages/cfa-template/template/frontend/src/actions/integrations.js
@@ -1,6 +1,6 @@
 export const SET_INTEGRATIONS = 'SET_INTEGRATIONS';
 
-// an action for setting the the integrations into redux
+// an action for setting the integrations into redux
 export function setIntegrations(integrations) {
     return {
         type: SET_INTEGRATIONS,
