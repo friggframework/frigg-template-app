b350c8a738e9c8ef9fb24aa2a3eb650fe9ac79ed
d-klotz <danielfelipeklotz@gmail.com>
2024-08-09 09:11:30 -0300

    fix: eslint

--- packages/cfa-template/template/frontend/eslint.config.js
+++ packages/cfa-template/template/frontend/eslint.config.js
@@ -1,11 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   {
+    extends: ['@friggframework/eslint-config'],
     files: ['**/*.{js,jsx}'],
     ignores: ['dist'],
     languageOptions: {
@@ -35,4 +36,4 @@ export default [
       ],
     },
   },
-]
+];
